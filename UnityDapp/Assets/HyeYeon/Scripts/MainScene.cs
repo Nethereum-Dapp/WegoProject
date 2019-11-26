380fe5b058563f9f4128fa6a1835b2a7d6c2d545
HyeYeonCha <gpdus335@naver.com>
2019-11-26 17:48:48 +0900

    made main scene menu tootip

--- UnityDapp/Assets/HyeYeon/Scripts/MainScene.cs
+++ UnityDapp/Assets/HyeYeon/Scripts/MainScene.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MainScene : MonoBehaviour
+public class MainScene : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public GameObject tooltip; // 툴팁 창
+    public Text tooltipTextUI; // 툴팁의 글씨 UI
+    public string tooltipText; // 툴팁에 띄울 글씨
+
+
     void Awake()
     {
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
@@ -30,4 +37,23 @@ public class MainScene : MonoBehaviour
     {
         SceneManager.LoadScene("LightningGame");
     }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tooltip.SetActive(true);
+        tooltip.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 400);
+        tooltipTextUI.text = TextEnterLine(tooltipText);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltip.SetActive(false);
+    }
+
+    private string TextEnterLine(string text)
+    {
+        text = text.Replace("\\n", "\n");
+        return text;
+    }
+
 }
